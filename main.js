21e5a7838e16a5e9fc61e37424b1b60890685668
Shirvan Khaligov <shirvan.khaligov@hq.ferrumcapital.az>
2021-10-19 22:57:17 +0400

    Task

---
+++ main.js
@@ -0,0 +1,25 @@
+// var dicty = document.getElementById("1");
+// console.log(dicty);
+
+// document.getElementsByTagName("div")[1].onclick = function (){
+//     (alert('Salam'));
+
+// }
+
+// function isEven(n) {
+//   if (n % 2 == 0) {
+//     console.log("Entered Number " + n + " this number is Even");
+//   } else {
+//     console.log("this number is not Even");
+//   }
+// }
+
+// console.log(isEven(32));
+// console.log(isEven(12));
+// console.log(isEven(337));
+// console.log(isEven(3256));
+
+function Add(n1, n2) {
+  return n1 + n2;
+}
+console.log(Add("12.25", "Salam"));
